354d9b9af0293efe80218812eb2ddefed18c440f
ZhaYanyu <Zha@ZhayYanyus-Mac-mini.local>
2017-08-30 15:23:30 +0800

    提交初版

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-native-test
+下载成功后请在根目录执行：
+ npm install
--- ios/testModel/AppDelegate.m
+++ ios/testModel/AppDelegate.m
@@ -18,8 +18,8 @@
 {
   NSURL *jsCodeLocation;
 
-  //  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-  jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.68:8081/index.ios.bundle?platform=ios&dev=true"];
+    jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+//  jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.68:8081/index.ios.bundle?platform=ios&dev=true"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"testModel"
